f26210c2f3f5383c6068b15fb922dc1e1374109f
Катерина Новикова <katherinov.o@gmail.com>
2023-02-19 17:59:23 +0300

    1 review

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -100,14 +100,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.UserRateThrottle',
-        'rest_framework.throttling.AnonRateThrottle',
-    ],
-    'DEFAULT_THROTTLE_RATES': {
-        'user': '1000/hour',
-        'anon': '100/hour',
-    }
 }
 
 SIMPLE_JWT = {
